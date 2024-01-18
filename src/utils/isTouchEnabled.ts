5e627aaa0614b8139e50ec850b7b616c7fcb4d9a
kadchenko <dmitry.kadchenko@gmail.com>
2024-01-18 22:53:22 +0200

    fix: window is not defined error

--- src/utils/isTouchEnabled.ts
+++ src/utils/isTouchEnabled.ts
@@ -1,3 +1,6 @@
 export function isTouchEnabled() {
-  return 'ontouchstart' in window || navigator.maxTouchPoints;
+  if (typeof window !== 'undefined') {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  }
+  return false;
 }
